5f855de61d34eb68b4e6ed2ac31e9f11e22068eb
Willian Tomaz Nieckarz <willian.nieckarz@icloud.com>
2023-10-18 23:23:32 -0300

    Diversos
    - Adicionado lembrete de como instalar os plugins do tmux
    - Trocado o icomplete-vertical-mode pelo Vertico
    - Adicionado Orderless
    - Adicionado ferramentas para Clojure
    - Adicionada configurações padrões do smartparens
    - Corrigida a indentação do arquivo
    - Adicionado Flycheck
    - Adicionado ao path a pasta do Emacs
    - Setado o locale pra UTF-8 no fish
    - Adicionado como editor padrão o emacsclient (vim que me perdoe)

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,11 @@
+set -x LC_ALL en_US.UTF-8
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
 alias config '/usr/bin/git --git-dir=$HOME/Projects/dotfiles --work-tree=$HOME/.config'
-fish_add_path /opt/local/bin/
+fish_add_path /opt/local/bin/ /Applications/MacPorts/EmacsMac.app/Contents/MacOS/bin/
 set -Ux LD_LIBRARY_PATH /opt/local/lib/ /opt/local/lib/gcc12/
+set -Ux EDITOR "emacsclient -t"
 source /opt/local/share/fzf/shell/key-bindings.fish
