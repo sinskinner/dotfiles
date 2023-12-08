84c854f96d094b5f702829e2924c1a419e0a62c5
Willian Tomaz Nieckarz <willian.nieckarz@icloud.com>
2023-12-08 13:11:53 -0300

    Adições aleatórias
    - Adicionado MOTD ao Fish
    - Adicionado cowsay e fortune
    - Alterado de smartparens para paredit
    - Incluído clj-refactor, lsp-ui, emacs-lsp, neotree
    - Removido flycheck-clojure
    - Alterado tema
    - Reformatado o config.org.

--- fish/config.fish
+++ fish/config.fish
@@ -4,8 +4,17 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+
+function fish_greeting
+    if status is-interactive
+        fortune | cowthink
+    end
+end
+
+
 alias config '/usr/bin/git --git-dir=$HOME/Projects/dotfiles --work-tree=$HOME/.config'
 fish_add_path /opt/local/bin/ /Applications/MacPorts/EmacsMac.app/Contents/MacOS/bin/
 set -Ux LD_LIBRARY_PATH /opt/local/lib/ /opt/local/lib/gcc12/
 set -Ux EDITOR "emacsclient -t"
 source /opt/local/share/fzf/shell/key-bindings.fish
+status --is-interactive; and jenv init - | source
