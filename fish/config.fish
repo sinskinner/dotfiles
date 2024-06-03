0995bc3876db7429bf30f582e7a41403d156e779
Willian Tomaz Nieckarz <willian.nieckarz@icloud.com>
2024-06-03 10:03:47 -0300

    Adiciona ajustes ao CIDER e ajustado temporariamente o EDITOR

--- fish/config.fish
+++ fish/config.fish
@@ -15,6 +15,6 @@ end
 alias config '/usr/bin/git --git-dir=$HOME/Projects/dotfiles --work-tree=$HOME/.config'
 fish_add_path /opt/local/bin/ /Applications/MacPorts/EmacsMac.app/Contents/MacOS/bin/
 set -Ux LD_LIBRARY_PATH /opt/local/lib/ /opt/local/lib/gcc12/
-set -Ux EDITOR "emacsclient -t"
+set -Ux EDITOR "vim"
 source /opt/local/share/fzf/shell/key-bindings.fish
 status --is-interactive; and jenv init - | source
